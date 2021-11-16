aaf1627f17f71e467e7998c6d4b204b7827e2665
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-11-16 14:30:59 -0600

    added prompt for title and desc

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
 
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
+const generatePage = require('./src/template.js');
+const inquirer = require('inquirer');
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project. (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +48,7 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+promptUser()
+.then(answers => console.log(answers));
